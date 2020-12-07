ca78d08934080fce84126b5efd7d058cfaeae383
talkhanz <talkhanz@users.noreply.github.com>
2020-12-07 22:51:05 +0500

    Updates

---
+++ precache-manifest.c66da8bbb062911e112d77750c298d18.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7e4db885f629297c37a6dc9193992a",
+    "url": "/react-app-cosmos-db/index.html"
+  },
+  {
+    "revision": "322d073a737316643c7a",
+    "url": "/react-app-cosmos-db/static/css/main.6c7cf6ac.chunk.css"
+  },
+  {
+    "revision": "8ef6e765cbe496fe1293",
+    "url": "/react-app-cosmos-db/static/js/2.b32b9ce0.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-app-cosmos-db/static/js/2.b32b9ce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "322d073a737316643c7a",
+    "url": "/react-app-cosmos-db/static/js/main.53bd413f.chunk.js"
+  },
+  {
+    "revision": "6272d81250ec2f33fa27",
+    "url": "/react-app-cosmos-db/static/js/runtime-main.36902e7e.js"
+  },
+  {
+    "revision": "1db1ea3bd1781c53043f00df1867007f",
+    "url": "/react-app-cosmos-db/static/media/banner.1db1ea3b.jpg"
+  },
+  {
+    "revision": "d28494a7e2d5a52dc6938dcf823aef05",
+    "url": "/react-app-cosmos-db/static/media/lipStick.d28494a7.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-cosmos-db/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ac8cec7b0ebeda82d3fd3ff9a5381c20",
+    "url": "/react-app-cosmos-db/static/media/makeUp.ac8cec7b.jpg"
+  }
+]);
